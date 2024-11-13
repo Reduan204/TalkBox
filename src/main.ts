87cb1fa950aa1a07d4a4c49dc23dd1cd89e1f4f2
abuxTM <abuxtmk1@gmail.com>
2024-11-13 08:04:52 +0000

    Fixed issues related to server and sending | LIL Cleanup | Display all the users from the server

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,36 @@ function toggleJoinPanel() {
   }
 }
 
+async function displayUsers() {
+  try {
+    const usernames: string[] = await invoke("get_users");
+    const usersContainer = document.getElementById("users-container");
+    
+    if (usersContainer) {
+      usersContainer.innerHTML = "";
+      usernames.forEach((username) => {
+        const userElement = document.createElement("p");
+        userElement.textContent = username;
+        usersContainer.appendChild(userElement);
+      });
+    }
+  } catch (error) {
+    console.error("Failed to fetch users:", error);
+  }
+}
+
+async function updateUserCount() {
+  try {
+    const userCount: string = await invoke("get_users_len");
+    const totalUsersElement = document.getElementById("total-users");
+    if (totalUsersElement) {
+      totalUsersElement.textContent = `Total Users: ${userCount}`;
+    }
+  } catch (error) {
+    console.error("Failed to fetch user count:", error);
+  }
+}
+
 async function createServer() {
   invoke("create_server")
     .then(() => console.log("Server created"))
@@ -28,11 +58,18 @@ async function joinServer() {
   const ip = serverIPInput.value;
 
   invoke("join_server", { ip, username })
-    .then(() => console.log("Joined server"))
+    .then(() => {
+      console.log("Joined server")
+      updateUserCount();
+      displayUsers();
+    })
     .catch((error) => console.error(error));
 }
 
 window.addEventListener("DOMContentLoaded", () => {
+  updateUserCount();
+  displayUsers();
+
   const createServerButton = document.querySelector("#create-server");
   createServerButton?.addEventListener("click", () => {
     createServer();
@@ -46,6 +83,8 @@ window.addEventListener("DOMContentLoaded", () => {
   const connectButton = document.getElementById("connect-btn");
   connectButton.addEventListener("click", () => {
     joinServer();
+    displayUsers();
     toggleJoinPanel();
   });
 });
+
